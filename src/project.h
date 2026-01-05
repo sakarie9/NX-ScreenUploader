138ee7cb07bd7302f8a27094e9f61ee92aff908f
Sakari <20642596+sakarie9@users.noreply.github.com>
2026-01-05 10:14:35 +0800

    bump: update version to 0.1.9

--- src/project.h
+++ src/project.h
@@ -3,9 +3,9 @@
 
 #define APP_TITLE "NX-ScreenUploader"
 #define APP_AUTHOR "Sakari"
-#define APP_VERSION  "0.1.8"
+#define APP_VERSION  "0.1.9"
 #define VERSION_MAJOR "0"
 #define VERSION_MINOR "1"
-#define VERSION_MICRO "8"
+#define VERSION_MICRO "9"
 
 #endif // INCLUDE_GUARD
